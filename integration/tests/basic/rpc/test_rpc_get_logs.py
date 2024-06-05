35de96da0da272bf68ac9fac6bfba0b95deb6aaf
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-06-05 13:40:37 +0100

    remove not-standard transactionLogIndex

--- integration/tests/basic/rpc/test_rpc_get_logs.py
+++ integration/tests/basic/rpc/test_rpc_get_logs.py
@@ -41,7 +41,6 @@ class TestRpcGetLogs:
         "address",
         "logIndex",
         "data",
-        "transactionLogIndex",
     ]
     ETH_BOOL_FIELDS = ["removed"]
     NEON_HASH_FIELDS = ["solanaTransactionSignature"]
